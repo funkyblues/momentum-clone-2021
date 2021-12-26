0900d2b262ab594d5fd8c252c0711da91b7026d9
funkyblues <dnr8874@naver.com>
2021-12-27 00:39:45 +0900

    5.1 Timeouts and Dates

--- js/clock.js
+++ js/clock.js
@@ -1,7 +1,14 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello() {
-  console.log("Hello");
+function getClock() {
+  const date = new Date();
+  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 
-setInterval(sayHello, 5000);
+getClock();
+setInterval(getClock, 1000);
+
+
+
+
+// Timeouts// 호출 함수를 넣어주고, 얼마나(ms) 기다릴 것인지를 입력
