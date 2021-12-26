d05c685249ca27c16a6cbe979ac104e0622a4a0c
funkyblues <dnr8874@naver.com>
2021-12-26 23:46:57 +0900

    4.6 Loading Username

--- app.js
+++ app.js
@@ -2,15 +2,32 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting =document.querySelector("#greeting");
 
+/*string을 반복해서 사용할 땐, 대문자로 const 정의해 주는 게 오타 예방에 도움이 됨.. */
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault(); 
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  localStorage.setItem("username", username);
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
+
+function paintGreetings(username) {
   greeting.innerText = `Hello, ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
-
 }
+
 loginForm.addEventListener("submit", onLoginSubmit);
+
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+}
+else {
+  paintGreetings(savedUsername);
+}
