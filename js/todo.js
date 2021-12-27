caa4a6b42b4ac7aeb8f529d7d074de662fc70cf3
funkyblues <dnr8874@naver.com>
2021-12-28 01:44:51 +0900

    TO DO LIST - 7.2 Deleting To Dos

--- js/todo.js
+++ js/todo.js
@@ -2,12 +2,24 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+  const todoLine = event.target.parentElement;
+  todoLine.remove();
+}
+
 /*JavaScript로 HTML 만든거임!!  */
 function paintToDo(newTodo) {
   const todoLine = document.createElement("li");
   const span = document.createElement("span");
-  todoLine.appendChild(span);
   span.innerText = newTodo;
+
+  const button = document.createElement("button");
+  button.innerText = "❌";
+
+  button.addEventListener("click", deleteToDo);
+
+  todoLine.appendChild(span);
+  todoLine.appendChild(button);
   toDoList.appendChild(todoLine);
 }
 
