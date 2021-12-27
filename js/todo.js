c076275154b18d322b7ab6d03a1d5d8c6c91b0f7
funkyblues <dnr8874@naver.com>
2021-12-28 01:21:46 +0900

    TO DO LIST - Deleting To Dos

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,20 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+/*JavaScript로 HTML 만든거임!!  */
+function paintToDo(newTodo) {
+  const todoLine = document.createElement("li");
+  const span = document.createElement("span");
+  todoLine.appendChild(span);
+  span.innerText = newTodo;
+  toDoList.appendChild(todoLine);
+}
+
 function handleToDoSubmit(event) {
   event.preventDefault();
-  const newToDo = toDoInput.value;
+  const newTodo = toDoInput.value;
   toDoInput.value = "";
+  paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
