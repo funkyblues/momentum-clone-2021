00987adbd78967667ac9814f67ca282c4b88248f
funkyblues <dnr8874@naver.com>
2021-12-27 23:05:20 +0900

    6.0 Quotes

---
+++ js/quotes.js
@@ -0,0 +1,51 @@
+const quotes = [
+  {
+    quote: "실 엉킨 것은 풀어도, 노 얽힌 것은 못 푼다.",
+    author: "속담",
+  },
+  {
+    quote: "폭풍이 지난 뒤에 고요가 온다.",
+    author: "속담",
+  },
+  {
+    quote:"인류를 위해 사는 것은 자기의 이름을 위해 사는 것 보다 훌륭한 것이다.",
+    author: "바첼 린드세이",
+  },
+  {
+    quote: "고난은 인간의 진가를 증명하는 것이다.",
+    author: "에피테토스",
+  },
+  {
+    quote: "가장 높은 곳에 올라가려면, 가장 낮은 곳부터 시작하라",
+    author: "푸블릴리우스시루스",
+  },
+  {
+    quote: "정직은 가장 확실한 자본이다.",
+    author: "랠프 윌도 에머슨",
+  },
+  {
+    quote: "겸손은 속옷과 같다. 입기는 입되 남에게 보이게는 입지 말라.",
+    author: "탈무드",
+  },
+  {
+    quote: "하나의 모범은 천 마디의 논쟁보다 더 가치 있는 것이다.",
+    author: "토마스 카알라일",
+  }, 
+  {
+    quote: "작은 도끼라도 찍고 찍으면 큰 참나무는 넘어진다.",
+    author: "셰익스피어",
+  },
+  {
+    quote: "사람의 척도는 그가 불행을 얼마나 잘 이겨내는지에 달려있다.",
+    author: "프르다크",
+  },
+]
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+
+const todayQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todayQuote.quote;
+author.innerText = todayQuote.author;
