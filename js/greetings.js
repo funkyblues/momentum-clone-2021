8b3858f6064bd9a13d9dc463a946f443f209637c
funkyblues <dnr8874@naver.com>
2022-01-04 00:53:14 +0900

    submit할 떄 자연스럽게 사라질 수 있도록.  - To Do 항목 나와야 함  - To Do리스트 나와야함

--- js/greetings.js
+++ js/greetings.js
@@ -15,7 +15,7 @@ function onLoginSubmit(event) {
 }
 
 function paintGreetings(username) {
-  greeting.innerText = `Hello, ${username}!`;
+  greeting.innerText = `Greetings, ${username}!`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
