2ec4d49a5124aef6afe954298e0cca15d17352fe
funkyblues <funkyblues@naver.com>
2022-01-05 15:47:41 +0900

    제출되면 사라지는거 확인해야함... 명언 가운데 정렬?

--- js/greetings.js
+++ js/greetings.js
@@ -1,14 +1,18 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const loginText = document.querySelector("#login-form .login__text");
 const greeting = document.querySelector("#greeting");
 
 /*string을 반복해서 사용할 땐, 대문자로 const 정의해 주는 게 오타 예방에 도움이 됨.. */
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
+const SUBMITTED = "submitted";
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 function onLoginSubmit(event) {
   event.preventDefault(); 
-  loginForm.classList.add(HIDDEN_CLASSNAME);
+  loginText.classList.add(SUBMITTED);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
   paintGreetings(username);
@@ -16,13 +20,12 @@ function onLoginSubmit(event) {
 
 function paintGreetings(username) {
   greeting.innerText = `Greetings, ${username}!`;
+  loginText.classList.add(SUBMITTED);
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-const savedUsername = localStorage.getItem(USERNAME_KEY);
-
 if (savedUsername === null) {
-  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginText.classList.remove(SUBMITTED);
   loginForm.addEventListener("submit", onLoginSubmit);
 }
 else {
